f63903fe1ba8944540bec9bf478fceed6b35e96f
wimaar2021daw2 <core.editor>
2021-10-29 18:34:40 +0200

    Actualización Match

--- src/classes/Match.py
+++ src/classes/Match.py
@@ -1,13 +1,15 @@
-import datetime
-
+import datetime, Team
 class Match:
     date = datetime.datetime.now()
-    localName = ""
-    visitorName = ""
+    local = Team("local")
+    visitor = Team("visitor")
 
-    def __init__(self, localName, visitorName):
-        self.localName = localName
-        self.visitorName = visitorName
+    def __init__(self, local, visitor):
+        self.local = local
+        self.visitor = visitor
 
     def setDate(self, year, month, day):
         self.date = datetime.datetime(year, month, day)
+
+    def matchResult(self):
+        return self.local.name + ":" + self.local.goals + "-" + self.visitor.name + ":" + self.visitor.goals
